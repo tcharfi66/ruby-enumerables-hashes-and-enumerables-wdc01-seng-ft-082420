7c43d6c50f31f1822168f6fe850d80f890adb52d
Tej Charfi <charfi@usc.edu>
2020-08-17 21:09:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do |key, value|
-    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
